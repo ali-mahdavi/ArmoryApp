bd4cd179f5f17f869499493a5185a9fb926d0021
unknown <alijob675@gmail.com>
2019-03-27 20:17:47 +0430

    Initial application generated by JHipster-5.8.2

---
+++ gateway/src/main/java/com/heloma/armory/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.heloma.armory.repository;
+
+import com.heloma.armory.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
---
+++ gateway/src/main/java/com/heloma/armory/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.heloma.armory.service.dto;
